96253fb804b47f842c54a34a127978d4b8054256
Sudhesh10 <rajaksudesh10@gmail.com>
2026-05-13 18:29:50 +0530

    Enable nodemailer debug

--- mailer.js
+++ mailer.js
@@ -26,6 +26,10 @@ function createTransporter() {
 
   return nodemailer.createTransport({
     service: 'gmail',
+
+    logger: true,
+    debug: true,
+
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS.replace(/\s/g, '')
